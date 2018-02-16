04feb53fd4e5af79dba7193787ab498ea5019092
mohammedsaadat <bbbb666111>
2018-02-16 19:01:00 +0000

    Rather than importing all the files from java.io, only import the ones that we actually need. Changes were made in the Words.java file.

--- src/cs5031/Words.java
+++ src/cs5031/Words.java
@@ -1,6 +1,9 @@
 package cs5031;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Words {
