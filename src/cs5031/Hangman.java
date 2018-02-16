e4c79d682feacb78b4c0a6d12d0a66d74a751dc5
mohammedsaadat <bbbb666111>
2018-02-16 19:48:37 +0000

    Fixed all of the indentation problems in the Hangman.java file with the help of intelji reformat code tool.

--- src/cs5031/Hangman.java
+++ src/cs5031/Hangman.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class Hangman {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        GameState game; CommandOpts opts;
-		boolean correct;
-		opts = new CommandOpts(args);
-		if (opts.getWordsource() == "") {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        GameState game;
+        CommandOpts opts;
+        boolean correct;
+        opts = new CommandOpts(args);
+        if (opts.getWordsource() == "") {
             System.out.println("  1. Counties");
             System.out.println("  2. Countries");
             System.out.println("  3. Cities");
@@ -17,25 +18,25 @@ public class Hangman {
             System.out.print("Pick a category:");
 
             game = new GameState(Words.randomWord(sc.nextInt()),
-                     opts.getMaxguesses(), opts.getMaxhints());
-		} else {
-			game = new GameState(Words.randomWord(opts.getWordsource()),
                     opts.getMaxguesses(), opts.getMaxhints());
-		}
-		while (!game.won() && !game.lost()) {
-			game.showWord();
-			System.out.println("Guesses remaining: " + game.getWrong());
+        } else {
+            game = new GameState(Words.randomWord(opts.getWordsource()),
+                    opts.getMaxguesses(), opts.getMaxhints());
+        }
+        while (!game.won() && !game.lost()) {
+            game.showWord();
+            System.out.println("Guesses remaining: " + game.getWrong());
             correct = game.guessLetter();
-			if (correct)
-			    System.out.println("Good guess!");
-			if (!correct)
-			    System.out.println("Wrong guess!");
-		}
-		if (game.won()) {
-			System.out.println("Well done!");
-			System.out.println("You took " + game.getG() + " guesses");
-		} else {
-			System.out.println("You lost! The word was " + game.getWrong());
-		}
-	}
+            if (correct)
+                System.out.println("Good guess!");
+            if (!correct)
+                System.out.println("Wrong guess!");
+        }
+        if (game.won()) {
+            System.out.println("Well done!");
+            System.out.println("You took " + game.getG() + " guesses");
+        } else {
+            System.out.println("You lost! The word was " + game.getWrong());
+        }
+    }
 }
