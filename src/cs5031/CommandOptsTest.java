9b514b170ec0f1b47f290c20976081283024defc
mohammedsaadat <bbbb666111>
2018-02-16 18:57:34 +0000

    Fixing some indentation problems in CommandOptsTest class

--- src/cs5031/CommandOptsTest.java
+++ src/cs5031/CommandOptsTest.java
@@ -5,10 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CommandOptsTest {
-
 	@Test
 	public void optionsTest() {
-		String[] args = { "--guesses", "2", "--hints", "4", "words.txt" };
+		String[] args = {"--guesses", "2", "--hints", "4", "words.txt"};
 		CommandOpts opts = new CommandOpts(args);
 		assertEquals(opts.getMaxguesses(), 2);
 		assertEquals(opts.getMaxhints(), 4);
