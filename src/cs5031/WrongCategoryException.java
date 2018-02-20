fcbb57c634edf4e25f80cbb593b230ab8b51b4cd
mohammedsaadat <bbbb666111>
2018-02-20 15:03:57 +0000

    Comments added to the WrongCategoryException class.

--- src/cs5031/WrongCategoryException.java
+++ src/cs5031/WrongCategoryException.java
@@ -1,6 +1,13 @@
 package cs5031;
 
+/**
+ * Exception class thrown whenever the input for the category is wrong.
+ */
 public class WrongCategoryException extends Exception {
+
+    /**
+     * Constructor that creates an instance of WrongCategoryException. 
+     */
     public WrongCategoryException() {
         super();
     }
