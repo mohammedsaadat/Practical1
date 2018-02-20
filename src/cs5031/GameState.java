04d7a9c740fcf52661d5c33b86f8c62b20d05c1b
mohammedsaadat <bbbb666111>
2018-02-20 21:00:10 +0000

    Comments and style changes made to some of the methods in GameState.
    JavaDoc comments added to some of the refactored bits of the GameState
    class. Style has been changed as well as per the check style plugin.

--- src/cs5031/GameState.java
+++ src/cs5031/GameState.java
@@ -3,53 +3,119 @@ package cs5031;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * This class handles all of the Hangman game logic.
+ */
 public class GameState {
+    /**
+     * String that represents the target word that the player
+     * has to guess.
+     */
     private String word;
+
+    /**
+     * Integer that represents the number of guesses that the
+     * player has taken.
+     */
     private int guesses;
+
+    /**
+     * Integer that represents the number of remaining guesses
+     * that the player has left.
+     */
     private int remainingGuesses;
+
+    /**
+     * Integer that represents the number of remaining hints
+     * that the player has left.
+     */
     private int remainingHints;
 
+    /**
+     * ArrayList that holds the letters that the player already
+     * guessed from the word.
+     */
     private ArrayList<Character> guessedLetters;
+
+    /**
+     * ArrayList that holds the letters that the player has not
+     * guessed from the word yet.
+     */
     private ArrayList<Character> unGuessedLetters;
 
+    /**
+     * Scanner used to get user input when the game is ran.
+     */
     private Scanner sc = new Scanner(System.in).useDelimiter("\n");
 
+    /**
+     * Constructor of GameState that initialises the target,
+     * maxGuesses and maxHints variables in the GameState class.
+     * @param target String that represents the target word.
+     * @param maxGuesses Integer that represents the maximum number of guesses.
+     * @param maxHints Integer that represents the maximum number of hints.
+     */
     public GameState(String target, int maxGuesses, int maxHints) {
         this.word = target;
         this.guesses = 0;
         this.remainingHints = maxHints;
         remainingGuesses = maxGuesses;
-
         unGuessedLetters = new ArrayList<>();
         guessedLetters = new ArrayList<>();
-
     }
 
-    public void initialiseUnGuessedArray(String word, ArrayList<Character> unGuessedLetters) {
-        for (int i = 0; i < word.length(); ++i) {
-            Character character = Character.toLowerCase(word.charAt(i));
-            if (!unGuessedLetters.contains(character)) {
-                unGuessedLetters.add(character);
+    /**
+     * Takes in a word and an array list and populates that array list with
+     * the word characters without duplicates.
+     * @param targetWord String that is the target word.
+     * @param wordCharacters ArrayList of string.
+     */
+    public void initialiseUnGuessedArray(String targetWord,
+                                         ArrayList<Character> wordCharacters) {
+        for (int i = 0; i < targetWord.length(); ++i) {
+            Character character = Character.toLowerCase(targetWord.charAt(i));
+            if (!wordCharacters.contains(character)) {
+                wordCharacters.add(character);
             }
         }
     }
 
+    /**
+     * Getter that returns the unGuessedLetters ArrayList.
+     * @return The unGuessedLetters ArrayList.
+     */
     public ArrayList<Character> getUnGuessedLetters() {
         return unGuessedLetters;
     }
 
+    /**
+     * Getter that returns the target word.
+     * @return String that represents the target word.
+     */
     public String getWord() {
         return word;
     }
 
+    /**
+     * Getter that returns the number of guesses taken so far.
+     * @return Integer that represents the number of guesses taken so far.
+     */
     public int getGuesses() {
         return guesses;
     }
 
+    /**
+     * Getter that returns the number of remaining guesses.
+     * @return Integer that represents the number of remaining guesses.
+     */
     public int getRemainingGuesses() {
         return remainingGuesses;
     }
 
+    /**
+     * Getter that returns the number of remaining hints.
+     * @return Integer that represents the number of remaining hints.
+     */
     public int getRemainingHints() {
         return remainingHints;
     }
